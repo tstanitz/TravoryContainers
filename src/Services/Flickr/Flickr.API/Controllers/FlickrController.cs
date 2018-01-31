d9725505c50b9ea1705cdfd232ea0cc5ba227706
Tamas Stanitz <tamas.stanitz@gmail.com>
2018-01-31 20:28:14 +0100

    Add route name

--- src/Services/Flickr/Flickr.API/Controllers/FlickrController.cs
+++ src/Services/Flickr/Flickr.API/Controllers/FlickrController.cs
@@ -6,7 +6,7 @@ using TravoryContainers.Services.Flickr.API.Model;
 
 namespace TravoryContainers.Services.Flickr.API.Controllers
 {
-    [Route("api/v1/flickr")]
+    [Route("flickr")]
     public class FlickrController : Controller
     {
         private readonly IFlickrConnector _flickrConnector;
@@ -17,7 +17,8 @@ namespace TravoryContainers.Services.Flickr.API.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> GetAlbums(UserData userData)
+        [Route("albums")]
+        public async Task<IActionResult> GetAlbums([FromBody]UserData userData)
         {
             var flickrResult = await _flickrConnector.GetPhotoSets(userData);
             var albums = new List<Album>();
