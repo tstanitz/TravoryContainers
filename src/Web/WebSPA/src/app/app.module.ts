8c164bfd71769b0c9133ed10379c5b925e67414e
Tamas Stanitz <tamas.stanitz@gmail.com>
2018-02-08 20:34:52 +0100

    Albums component

--- src/Web/WebSPA/src/app/app.module.ts
+++ src/Web/WebSPA/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AlbumsComponent } from './albums/albums.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumsComponent
   ],
   imports: [
     BrowserModule
