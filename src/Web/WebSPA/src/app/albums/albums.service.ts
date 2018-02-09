9961f551763c0a42847891c1aaa12de6bc75b10f
Tamas Stanitz <tamas.stanitz@gmail.com>
2018-02-09 16:54:47 +0100

    Get data from environment

--- src/Web/WebSPA/src/app/albums/albums.service.ts
+++ src/Web/WebSPA/src/app/albums/albums.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
-import {Album} from './album';
+import { Album } from './album';
 
 @Injectable()
 export class AlbumsService {
@@ -10,7 +11,11 @@ export class AlbumsService {
   constructor(private http: HttpClient) { }
 
   getAlbums(): Observable<Album[]> {
-    return this.http.post<Album[]>("http://localhost:5000/api/v1/flickr/albums", {});
+    return this.http.post<Album[]>("http://localhost:5001/api/v1/flickr/albums", {
+      "consumerKey": environment.consumerKey,
+      "consumerSecret": environment.consumerSecret,
+      "token": environment.token,
+      "tokenSecret": environment.tokenSecret});
   }
 }
 
