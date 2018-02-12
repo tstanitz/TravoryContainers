8bd5086fae518d292577e3d39c827bc23b784878
Tamas Stanitz <tamas.stanitz@gmail.com>
2018-02-12 21:43:32 +0100

    add default route

--- src/Web/WebSPA/src/app/app-routing.module.ts
+++ src/Web/WebSPA/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AlbumsComponent} from './albums/albums.component';
 
 const routes: Routes = [
+  { path: '', redirectTo:'/albums', pathMatch: 'full'},
   { path: 'albums', component: AlbumsComponent }
 ];
 
